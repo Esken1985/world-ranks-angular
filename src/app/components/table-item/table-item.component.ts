cbcaa4ef5789171a9a2b2d5852a8e0a563bef92b
Eskender <esken999@gmail.com>
2020-11-30 16:31:25 +0300

    sorting by asc/desc population

--- src/app/components/table-item/table-item.component.ts
+++ src/app/components/table-item/table-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CountriesService } from 'src/app/services/countries.service';
 
 @Component({
@@ -6,22 +6,36 @@ import { CountriesService } from 'src/app/services/countries.service';
   templateUrl: './table-item.component.html',
   styleUrls: ['./table-item.component.scss']
 })
+
+
 export class TableItemComponent implements OnInit {
 
   countries: any = [];
+  orderedCountries: any[] = [];
+  direction: string = "asc";
   totalRecords: any;
   page: any = 1;
-
-  constructor(private _countriesService: CountriesService) { }
-
   
-
+  constructor(private _countriesService: CountriesService) {
+  }
+  orderBy(countries: any[], direction: string){
+    if(direction === "desc") {
+      return [...countries].sort((a, b) => (a.population > b.population ? 1 : -1));
+    }
+    if(direction === "asc") {
+      return [...countries].sort((a, b) => (a.population > b.population ? -1 : 1));
+    }
+    return countries
+  };
+  
   ngOnInit(): void {
     this._countriesService.getCountries()
-        .subscribe(data => {
-          this.countries = data
-          this.totalRecords = 250
-        });
+    .subscribe(data => {
+      this.countries = data
+      this.totalRecords = data.length
+      this.orderedCountries = this.orderBy(this.countries, "asc")
+    });
+    
   }
 
 }
